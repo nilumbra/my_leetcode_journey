bd7555e2b9b034aaa55c1b2a6b0bd68ea9034f1f
Mike Chen(HuaXiao) <478222482@qq.com>
2022-03-27 05:45:47 +0900

    Time: 134 ms (34.38%), Space: 14.2 MB (86.56%) - LeetHub

--- 311-sparse-matrix-multiplication/311-sparse-matrix-multiplication.py
+++ 311-sparse-matrix-multiplication/311-sparse-matrix-multiplication.py
@@ -1,22 +1,41 @@
 import functools
 class Solution:
     def multiply(self, mat1: List[List[int]], mat2: List[List[int]]) -> List[List[int]]:
-        res = [0] * len(mat1) 
-        for idx, row in enumerate(mat1):
-            res[idx] = self.row_comb(row, mat2)
+        m = len(mat1)
+        k = len(mat2)
+        n = len(mat2[0])
+        
+        res = []
+        compressed1 = self.compress_zeros(mat1) 
+        compressed2 = self.compress_zeros(mat2)
+        for i in range(m):
+            res.append(self.row_comb_sparse(mat1[i], mat2, compressed1[i], compressed2))
         
         return res
-        
-    def row_comb(self, row, mat):
+                              
+    def row_comb_sparse(self, row, mat2, comp_row, comp2):
+        pos = {}
+        for i in comp_row:
+            for j in comp2[i]:
+                if j not in pos:
+                    pos[j] = [i]
+                else:
+                    pos[j].append(i)
+
+        resrow = [0] * len(mat2[0])
+        for j in pos:
+            s = 0 
+            for i in pos[j]:
+                s += row[i] * mat2[i][j]
+            resrow[j] = s
+
+        return resrow
+                              
+    def compress_zeros(self, mat):
         """
-        Assumes row is a row vector and mat2 is a matrix where len(row) == len(mat2)
-        Returns the linear combination of mat2's rows weighted by row
+        Example:
+        7 0 0              [[0],
+        0 0 0   returns     [],
+        0 0 1              [2]]
         """
-        return functools.reduce(lambda a, b: [x + y for x, y in zip(a, b)], 
-                                [self.scale(f, r) for f, r in zip(row, mat)])
-            
-    def scale(self, s, v):
-        """
-        Returns the result of multiply vector v by scalar s
-        """
-        return list(map(lambda e: e * s ,v))
+        return [[idx for idx, e in enumerate(row) if e != 0] for row in mat]  
