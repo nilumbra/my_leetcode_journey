2c623e5dc6b93ffa2de195d08bc7cbf198c7d312
Mike Chen(HuaXiao) <478222482@qq.com>
2022-03-28 02:10:51 +0900

    Time: 106 ms (60.52%), Space: 13.7 MB (53.72%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
@@ -1,7 +1,7 @@
 import heapq
 import bisect
 class Solution(object):
-    def kWeakestRows(self, mat, k):
+    def kWeakestRows(self, mat, k): 
         """
         :type mat: List[List[int]]
         :type k: int
@@ -9,13 +9,14 @@ class Solution(object):
         """
         h = []
         l = len(mat[0])
-        for (idx, row) in enumerate(mat):
-            ones = l - bisect.bisect_left(row[::-1], 1)
-            heapq.heappush(h, (ones, idx))
+        for (idx, row) in enumerate(mat):  # m * log(nk) ==> the upperbound
+            ones = l - bisect.bisect_left(row[::-1], 1) # log(n)
+            heapq.heappush(h, (ones, idx)) # log(k)
         
         ans = []
-        for i in range(k):
+        for i in range(k): # O(k * log(k))
             ans.append(heapq.heappop(h)[1])
         
         return ans
             
+    
