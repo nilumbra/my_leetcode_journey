f40c90b7b09a801718cf61e541b6f24a1ffb4122
Mike Chen(HuaXiao) <478222482@qq.com>
2022-04-02 22:17:29 +0900

    Time: 2646 ms (42.86%), Space: 50.1 MB (57.14%) - LeetHub

---
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.ts
@@ -0,0 +1,41 @@
+type Point = [number, number]
+
+function maxDistance(grid: number[][]): number {
+	let max = -1
+	const lands: Point[] = []
+	const size = grid[0].length
+
+	for (let row = 0; row < size; row++) {
+		for (let col = 0; col < size; col++) {
+			if (grid[row][col]) { lands.push([row, col]) }
+		}
+	}
+
+	if (lands.length === 0) { return max }
+
+	for (let row = 0; row < size; row++) {
+		for (let col = 0; col < size; col++) {
+			if (grid[row][col] === 0) {
+				const nearest = nearestLand(lands, [row, col])
+				if (nearest > max) { max = nearest }
+			}
+		}
+	}
+
+	return max
+}
+
+function nearestLand(lands: Point[], point: Point) {
+	let nearest = Number.MAX_VALUE
+
+	for (const land of lands) {
+		const dist = distance(land, point)
+		if (dist < nearest) { nearest = dist }
+	}
+
+	return nearest
+}
+
+function distance(a: Point, b: Point) {
+	return Math.abs(b[0] - a[0]) + Math.abs(b[1] - a[1])
+}
