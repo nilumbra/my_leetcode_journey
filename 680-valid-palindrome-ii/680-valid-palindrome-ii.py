974f4f6cedaf5a271ea92df126f7f0edce002e5e
Mike Chen(HuaXiao) <478222482@qq.com>
2022-04-03 02:03:33 +0900

    Time: 199 ms (45.74%), Space: 14.6 MB (52.23%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.py
@@ -4,25 +4,41 @@ class Solution:
         :type s: str
         :rtype: bool
         """
+        def isP(s, i, j):
+            while(i < j):
+                if(s[i] != s[j]):
+                    return False
+                i += 1
+                j -= 1
+            
+            return True
+        
         left, right = 0, len(s) - 1
-        threshold = 1
-        err = 0
-        
-        def isP(s):
-            nonlocal err
-            nonlocal threshold
-            if len(s) <= 1:
-                return True
+        # threshold = 1
+        # err = 0
+        while left < right:
+            if (s[left] == s[right]):
+                left += 1
+                right -= 1
             else:
-                if s[0] == s[-1]:
-                    return isP(s[1:-1])
-                else:
-                    err += 1
-                    if err > threshold:
-                        return False
-                    else:
-                        return isP(s[1:]) or isP(s[:-1])
+                return isP(s, left+1, right) or isP(s, left, right-1)
+            
+        return True
         
+#         def isP(s):
+#             nonlocal err
+#             nonlocal threshold
+#             if len(s) <= 1:
+#                 return True
+#             else:
+#                 if s[0] == s[-1]:
+#                     return isP(s[1:-1])
+#                 else:
+#                     err += 1
+#                     if err > threshold:
+#                         return False
+#                     else:
+#                         return isP(s[1:]) or isP(s[:-1])
         
-        return isP(s)
+    
     
