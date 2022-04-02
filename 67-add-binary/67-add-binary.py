85160dac2c6a898a93ce983e261311b007407c5e
Mike Chen(HuaXiao) <478222482@qq.com>
2022-04-03 02:37:54 +0900

    Time: 32 ms (47.65%), Space: 13.6 MB (21.04%) - LeetHub

---
+++ 67-add-binary/67-add-binary.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        a, b = int(a, 2), int(b, 2)
+        return bin(a+b).replace("0b", "")
