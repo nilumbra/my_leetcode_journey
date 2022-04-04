19e2c370bee682808ffbe3a05238a1bdf3060cf7
Mike Chen(HuaXiao) <478222482@qq.com>
2022-04-05 06:26:24 +0900

    Time: 119 ms (76.14%), Space: 48.1 MB (99.02%) - LeetHub

---
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.js
@@ -0,0 +1,42 @@
+/**
+ * @param {number[][]} graph
+ * @return {number[][]}
+ */
+var allPathsSourceTarget = function(graph) {
+    if (graph[0].length == 0 ) return []
+
+    const visited = {}, 
+            ans = [],
+          stack = [];
+
+    stack.push(0)
+
+    while (stack.length != 0) {
+        
+        var cur = stack.at(-1);
+
+        if (!visited.hasOwnProperty(cur)) {
+            visited[cur] = 0;
+        } else {
+            visited[cur]++;
+        }
+
+        if (!graph[cur][visited[cur]] && graph[cur].length == visited[cur]) { // has visited all outcoming edes from the cur_node
+            if (cur == graph.length - 1) {
+                // console.log(stack)
+                ans.push(Array.from(stack))
+                // console.log(ans)
+            }
+            // console.log(visited)
+            delete visited[cur]
+            stack.pop()
+        } else {
+            var next_node = graph[cur][visited[cur]];
+            stack.push(next_node)
+        }
+        
+        // console.log(stack)
+    }
+
+    return ans
+}
