42959a5b2ab3bd5b3bba82b947a411133414ad7c
Mike Chen(HuaXiao) <478222482@qq.com>
2022-04-06 03:20:09 +0900

    Time: 105 ms (52.95%), Space: 50.1 MB (18.74%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.js
+++ 11-container-with-most-water/11-container-with-most-water.js
@@ -6,36 +6,19 @@ var maxArea = function(height) {
     
     let left = 0,
         right = height.length - 1,
-        leftmax = left,
-        rightmax = right,
-        waterContained = (height, l, r) => Math.min(height[l], height[r]) * (r - l);
+        waterContained = (height, l, r) => Math.min(height[l], height[r]) * (r - l),
+        maxArea = waterContained(height, left, right);
     
     
     while (left < right) {
-        // console.log(height[left], height[right])
-        // console.log((waterContained(height, left, right)))
-        // console.log(height[left+1], height[right-1])
-        // console.log("----")
         if (height[left] < height[right]) {
             left++;
-            // console.log(waterContained(height, left, rightmax), waterContained(height, leftmax, rightmax))
-            if(waterContained(height, left, rightmax) > waterContained(height, leftmax, rightmax)) {
-                leftmax = left;
-            }
-            
         } else {
             right--;
-            // console.log(waterContained(height, left, rightmax), waterContained(height, leftmax, rightmax))
-            if(waterContained(height, leftmax, right) > waterContained(height, leftmax, rightmax)) {
-                rightmax = right;
-            }
         }
         
-        if(waterContained(height, left, right) > waterContained(height, leftmax, rightmax)) {
-            rightmax = right,
-            leftmax = left
-        }
+        let area = waterContained(height, left, right);
+        maxArea = area > maxArea ? area: maxArea;
     }
-    
-    return waterContained(height, leftmax, rightmax)
+    return maxArea
 };
