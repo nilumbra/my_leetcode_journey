fbafd701bf4f16d01011b371b2a132fae9b60701
Mike Chen(HuaXiao) <478222482@qq.com>
2022-04-10 17:28:17 +0900

    Time: 1416 ms (6.59%), Space: 44.7 MB (17.18%) - LeetHub

--- 847-shortest-path-visiting-all-nodes/847-shortest-path-visiting-all-nodes.py
+++ 847-shortest-path-visiting-all-nodes/847-shortest-path-visiting-all-nodes.py
@@ -1,26 +1,25 @@
 class Solution:
-    def shortestPathLength(self, graph: List[List[int]]) -> int:
-        if len(graph) == 1:
-            return 0
-        
+    def shortestPathLength(self, graph):
+        def dp(node, mask):
+            state = (node, mask)
+            if state in cache:
+                return cache[state]
+            if mask & (mask - 1) == 0:
+                # Base case - mask only has a single "1", which means
+                # that only one node has been visited (the current node)
+                return 0
+
+            cache[state] = float("inf") # Avoid infinite loop in recursion
+            for neighbor in graph[node]:
+                if mask & (1 << neighbor):
+                    already_visited = 1 + dp(neighbor, mask)
+                    not_visited = 1 + dp(neighbor, mask ^ (1 << node))
+                    cache[state] = min(cache[state], already_visited, not_visited)
+
+            return cache[state]
+
         n = len(graph)
         ending_mask = (1 << n) - 1
-        queue = [(node, 1 << node) for node in range(n)]
-        seen = set(queue)
+        cache = {}
 
-        steps = 0
-        while queue:
-            next_queue = []
-            for i in range(len(queue)):
-                node, mask = queue[i]
-                for neighbor in graph[node]:
-                    next_mask = mask | (1 << neighbor)
-                    if next_mask == ending_mask:
-                        return 1 + steps
-                    
-                    if (neighbor, next_mask) not in seen:
-                        seen.add((neighbor, next_mask))
-                        next_queue.append((neighbor, next_mask))
-            
-            steps += 1
-            queue = next_queue
+        return min(dp(node, ending_mask) for node in range(n))
