9f6ab2d96e51aaef77f19e96d23e0c27077fdcea
Mike Chen(HuaXiao) <478222482@qq.com>
2022-04-13 13:21:15 +0900

    Time: 143 ms (90.32%), Space: 212.7 MB (29.03%) - LeetHub

--- 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.rb
+++ 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.rb
@@ -12,11 +12,8 @@ def nearest_valid_point(x, y, points)
             if point[0] == x && point[1] == y
                 return idx
             end
-            if point[0] == x && manh < min
-                min = manh
-                min_idx = idx
-            end
-            if point[1] == y && manh < min
+            
+            if manh < min
                 min = manh
                 min_idx = idx
                 # print ((point[0] - x).abs + (point[1] - y).abs), min_idx
