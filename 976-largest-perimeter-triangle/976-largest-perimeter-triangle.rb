b1fd2d15f0601a686271252027f229c248cfe2d2
Mike Chen(HuaXiao) <478222482@qq.com>
2022-04-13 11:57:46 +0900

    Time: 94 ms (83.78%), Space: 213.2 MB (86.49%) - LeetHub

--- 976-largest-perimeter-triangle/976-largest-perimeter-triangle.rb
+++ 976-largest-perimeter-triangle/976-largest-perimeter-triangle.rb
@@ -3,7 +3,6 @@
 def largest_perimeter(nums)
     
     nums.sort!.reverse!
-    print nums
     (nums.length - 2).times do |idx| 
         if nums[idx] < nums[idx+1] + nums[idx+2] 
             return nums[idx] + nums[idx+1] + nums[idx+2] 
