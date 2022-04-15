f3431cdfab12520a985c29603a67ff354dcb41d0
Mike Chen(HuaXiao) <478222482@qq.com>
2022-04-15 14:39:16 +0900

    Time: 58 ms (96.99%), Space: 43.6 MB (29.12%) - LeetHub

---
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.ts
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function middleNode(head: ListNode | null): ListNode | null {
+    // if (head.next == undefined) return head
+    var count: number = 1, 
+        ptr: ListNode = null,
+        mid: ListNode = null,
+        alt: boolean = true;
+    
+    ptr = head,
+    mid = head;
+
+    while (ptr.next) {
+        if (alt) mid = mid.next //((Math.floor(count / 2) + 1) ^ count) & 1
+        alt = !alt
+        ptr = ptr.next
+        count++;
+    }
+        
+    return mid
+};
