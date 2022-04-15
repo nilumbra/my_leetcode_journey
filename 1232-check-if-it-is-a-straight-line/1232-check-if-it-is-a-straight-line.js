7dea066ddbe3a75336397675edad99acfe5a0faa
Mike Chen(HuaXiao) <478222482@qq.com>
2022-04-15 12:08:18 +0900

    Time: 103 ms (22.79%), Space: 46.9 MB (5.09%) - LeetHub

--- 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.js
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.js
@@ -3,16 +3,13 @@
  * @return {boolean}
  */
 var checkStraightLine = function(coordinates) {
-    // coordinates.sort()
-    console.log(coordinates)
     const k = (p2, p1) => { 
         const k = (p2[1] - p1[1]) / (p2[0] - p1[0]);
         return Math.abs(k) == Infinity ? Infinity : k
     },
           slope = k(coordinates[1], coordinates[0]);
-    
+
     for (let i = 2; i < coordinates.length; i++) {
-        // console.log(`${last_p}, ${p}, ${diff2d(last_p, p)}, ${fixed}`)
         console.log(k(coordinates[i], coordinates[i-1]))
         if (k(coordinates[i], coordinates[i-1]) != slope) return false
     }
