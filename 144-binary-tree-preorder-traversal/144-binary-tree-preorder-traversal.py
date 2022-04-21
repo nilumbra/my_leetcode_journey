0462e199cb16f04813d2d15182bc5cf574824b2f
Mike Chen(HuaXiao) <478222482@qq.com>
2022-04-21 12:37:51 +0900

    Time: 17 ms (77.78%), Space: 13.4 MB (48.76%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.py
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.py
@@ -1,16 +1,40 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        def pre(root, ans):
-            if root:
-                ans.append(root.val)
-                pre(root.left, ans)
-                pre(root.right, ans)
-        ans = []
-        pre(root, ans)
-        return ans
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        # Is left child of the node is None? No. Then go left.
+        # Go right till you meet None or current node(t)
+        
+        # MEET NONE:
+        # 1. Add node to output
+        # 2. Link predecessor.right = node 
+        # 3. t = t.l
+        out = []
+        while root:
+            ##!! If root has left tree, root will be visited twice
+            if not root.left:
+                out.append(root.val)
+                root = root.right
+            else:
+                ptr = root.left
+                while ptr.right and ptr.right != root: 
+                    ptr = ptr.right
+                if not ptr.right: # root has a left tree and root has not been visited before
+                    out.append(root.val)
+                    ptr.right = root
+                    root = root.left
+                else: # ptr.right == t
+                    ptr.right = None
+                    root = root.right
+
+        return out
+                
+        
