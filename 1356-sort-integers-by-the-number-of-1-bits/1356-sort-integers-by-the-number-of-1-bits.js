61d801adaaccff7135da7cf8f5dbb119f0d2e14b
Mike Chen(HuaXiao) <478222482@qq.com>
2022-04-21 14:02:05 +0900

    Time: 92 ms (88.06%), Space: 44.6 MB (72.90%) - LeetHub

---
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+var sortByBits = function(arr) {
+    
+    function count1s(num) {
+        var count = 0;
+        while (num) {
+            if (num & 1) count++;
+            num >>= 1
+        }
+        
+        return count
+    }
+    
+    function compareFunc(a, b) {
+        // return > 0, sort b before a
+        const bitDiff = count1s(a) - count1s(b)
+        if (bitDiff != 0) {
+            return bitDiff
+        } else {
+            return a - b
+        }
+    }
+    
+    arr.sort(compareFunc)
+    return arr
+};
