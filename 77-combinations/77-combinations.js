0b0d6766a72c5a94ec9048b98418850852827174
Mike Chen(HuaXiao) <478222482@qq.com>
2022-05-08 00:41:56 +0900

    Time: 146 ms (61.36%), Space: 47.6 MB (84.31%) - LeetHub

--- 77-combinations/77-combinations.js
+++ 77-combinations/77-combinations.js
@@ -7,10 +7,12 @@ var combine = function(n, k) {
     let output = [];
     function backtrack(startIndex, curr_combination) {
         if (curr_combination.length === k) {
-           output.push(curr_combination);
+           output.push(curr_combination.slice());
         } else {
            for (let i = startIndex; i <= n; i++) {
-                backtrack(i + 1, curr_combination.concat([i]));
+                curr_combination.push(i)
+                backtrack(i + 1, curr_combination);
+                curr_combination.pop()
            }
         }
     }
