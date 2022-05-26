8b2b730a2a70805d63a05cd56e2ad8eac61ef287
Mike Chen(HuaXiao) <478222482@qq.com>
2022-05-27 03:00:23 +0900

    Time: 496 ms (7.12%), Space: 77.3 MB (19.48%) - LeetHub

---
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+var eraseOverlapIntervals = function(intervals) {
+    intervals.sort((a, b) => (a[1] - b[1]));
+    console.log(intervals)
+    var removeCnt = 0,
+        prevLast = -Infinity;
+    
+    for (const interval of intervals) {
+        if (interval[0] >= prevLast) { // e.g. prevLast: 10, interval: [11, 13]
+            prevLast = interval[1]
+            // console.log(interval, "no remove")
+        } else {
+            // e.g. prevLast: 10, interval: [9, 13]
+            // console.log(interval, "remove")
+            removeCnt++;  
+        }
+        
+        // console.log(prevLast)
+    }
+    
+    return removeCnt;
+};
