ac75d4a4d5ec37bec9657b2b0e2b6361b5f1a10c
Mike Chen(HuaXiao) <478222482@qq.com>
2022-05-26 17:37:22 +0900

    Time: 98 ms (30.87%), Space: 42.8 MB (83.32%) - LeetHub

---
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    var left = 0, 
+        right = numbers.length - 1;
+    
+    while (left < right) {
+        if (numbers[left] + numbers[right] > target) {
+            right--;
+        } else if (numbers[left] + numbers[right] < target) {
+            left++;
+        } else {
+            return [left + 1, right + 1]
+        }
+    }
+};
