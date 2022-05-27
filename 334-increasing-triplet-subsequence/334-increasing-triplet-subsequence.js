9aea8cff30a9f0a8e6e62aadd3730d968bea49c8
Mike Chen(HuaXiao) <478222482@qq.com>
2022-05-27 19:28:46 +0900

    Time: 157 ms (10.18%), Space: 56 MB (12.53%) - LeetHub

--- 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.js
+++ 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.js
@@ -7,35 +7,21 @@
 // If curr element is greater than the last, append
 // else if it is smaller than the last one, 
 
-var isIncreasing = function (arr) {
-    if (arr.length === 1 || (arr.length === 2 && arr[0] < arr[1])) {
-        return true
-    }
-    return false;
-}
 
 var increasingTriplet = function(nums) {
-    const ans = [nums[0]];
-    for (const num of nums.slice(1)) {
-        if (num > ans.at(-1) ) { // append
-            ans.push(num);
-            if (ans.length === 3) {
-                return true
-            }
-        } else if (num < ans.at(-1)) {
-            // update the last element greedily
-            if (ans.length === 2) {
-                // ans.length = 2 && ans[-2] < num
-                if (num < ans.at(-2)) {  // e.g. ans = [2, 3], num = 1
-                    ans[0] = num;
-                } else if (num < ans[1] && num > ans[0]) { // e.g. ans = [1, 3], num = 2
-                    ans[1] = num;    
-                }
-            } else { // ans.length == 1
-                ans[0] = num;
-            }
+    var first = Infinity,
+        second = Infinity;
+    
+    for (const num of nums) {
+        if (num <= first) {
+            first = num;
+        } else if (num <= second) {
+            second = num;
+        } else {
+            return true
         }
-        // console.log(ans)
+        
+        // console.log(first, second)
     }
     
     return false;
