8740801c440c2f039951e2b0e2d7081ab42fe583
Mike Chen(HuaXiao) <478222482@qq.com>
2022-05-28 19:01:40 +0900

    Time: 119 ms (13.40%), Space: 44.8 MB (16.32%) - LeetHub

---
+++ 409-longest-palindrome/409-longest-palindrome.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestPalindrome = function(s) {
+    var getAllRepetitiveLetterPair = function(s) {
+        const tab = {}
+        for (const c of s) {
+            // const k = c.charCodeAt(0);
+            if (typeof tab[c] === 'undefined') {
+                tab[c] = 1;
+            } else {
+                tab[c] += 1;
+            }
+        }    
+        return tab;
+    }
+    
+    const t = getAllRepetitiveLetterPair(s);
+    // console.log(t)
+    var palindromeCnt = 0;
+    for (const k in t) {
+        const cnt = t[k];
+        if (cnt >= 2) {
+            palindromeCnt += Math.floor(cnt / 2) * 2;
+        }
+    }
+    
+    console.log(palindromeCnt)
+    palindromeCnt += (palindromeCnt < s.length);
+    return palindromeCnt
+};
