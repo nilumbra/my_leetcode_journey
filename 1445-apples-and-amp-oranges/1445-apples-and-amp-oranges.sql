a8afbeb2f3e923122dd6df864768c3988be2c13b
Mike Chen(HuaXiao) <478222482@qq.com>
2022-06-05 23:40:46 +0900

    Time: 386 ms (29.75%), Space: 0B (100.00%) - LeetHub

--- 1445-apples-and-amp-oranges/1445-apples-and-amp-oranges.sql
+++ 1445-apples-and-amp-oranges/1445-apples-and-amp-oranges.sql
@@ -1,15 +1,15 @@
 # Write your MySQL query statement below
 
-# with 
-#     a as (select * from Sales where fruit = 'apples'),
-#     o as (select * from Sales where fruit = 'oranges')
-# select a.sale_date, sum(a.sold_num - o.sold_num) diff
-# from a join o
-# on a.sale_date = o.sale_date
-# group by 1;
+with 
+    a as (select * from Sales where fruit = 'apples'),
+    o as (select * from Sales where fruit = 'oranges')
+select a.sale_date, sum(a.sold_num - o.sold_num) diff
+from a join o
+on a.sale_date = o.sale_date
+group by 1;
 
-select sale_date,
-sum(case when fruit = 'apples' then sold_num end) -
-sum(case when fruit = 'oranges' then sold_num end) diff
-from Sales
-group by 1
+# select sale_date,
+# sum(case when fruit = 'apples' then sold_num end) -
+# sum(case when fruit = 'oranges' then sold_num end) diff
+# from Sales
+# group by 1
