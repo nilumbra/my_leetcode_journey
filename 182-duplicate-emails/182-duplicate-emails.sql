a12fcea0a6ac6304189d46bcf2096bae5511eb9a
Mike Chen(HuaXiao) <478222482@qq.com>
2022-06-06 18:03:16 +0900

    Time: 319 ms (82.81%), Space: 0B (100.00%) - LeetHub

--- 182-duplicate-emails/182-duplicate-emails.sql
+++ 182-duplicate-emails/182-duplicate-emails.sql
@@ -6,9 +6,10 @@
 #      group by email
 #      having count(*) > 1) e;
  
- select email
- from
-(select email, count(email) as q
-     from Person
-     group by email
-     having q > 1) e;
+ select email from
+(
+  select email, count(email) as q
+  from Person
+  group by email
+)  e
+ where q > 1;
