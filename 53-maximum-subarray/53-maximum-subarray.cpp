390abacfbdd8f71631acabe1663fd9fc1bbb9366
Mike Chen(HuaXiao) <478222482@qq.com>
2022-10-03 10:22:24 +0900

    Time: 141 ms (77.87%), Space: 70.5 MB (6.15%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        vector<int> f(nums.size());
+        int mT = nums[0];
+        int maxSum = mT;
+        for (int i = 1; i < nums.size(); ++i) {
+            mT = max(mT + nums[i], nums[i]);
+            maxSum = max(mT, maxSum);
+        }
+        return maxSum;
+    }
+};
