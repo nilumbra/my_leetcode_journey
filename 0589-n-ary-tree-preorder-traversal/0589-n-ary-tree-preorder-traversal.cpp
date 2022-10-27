c6fdd9c74fede89f5e744c169afa62f5478966ff
Mike Chen(HuaXiao) <478222482@qq.com>
2022-10-28 01:15:48 +0900

    Time: 26 ms (86.83%), Space: 11.4 MB (27.09%) - LeetHub

--- 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.cpp
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.cpp
@@ -18,18 +18,44 @@ public:
 };
 */
 
+// Recursive
+// class Solution {
+// public:
+//     vector<int> ans;
+//     void traverse(Node* root) {
+//       if (root) {
+//         ans.push_back(root->val);
+//         for (Node* child: root->children) // for in
+//           traverse(child);
+//       }
+//     }
+//     vector<int> preorder(Node* root) {
+//         traverse(root);
+//         return ans;
+//     }
+// };
+
+// Iterative
 class Solution {
 public:
     vector<int> ans;
-    void traverse(Node* root) {
-      if (root) {
-        ans.push_back(root->val);
-        for (Node* child: root->children) 
-          traverse(child);
-      }
-    }
+    stack<Node*> stack;
     vector<int> preorder(Node* root) {
-        traverse(root);
+        if (!root) return {};
+        
+        Node* t;
+        stack.push(root);
+        while(!stack.empty()) {
+          t = stack.top();
+          stack.pop();
+          
+          ans.push_back(t->val); // add to ans
+          
+          // push stack from right to left, so they leave from left to right
+          for(int i = t->children.size() - 1; i >= 0; --i) 
+            stack.push(t->children[i]);
+        }
+        
         return ans;
     }
 };
