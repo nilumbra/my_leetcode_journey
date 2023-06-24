55671acacda826086b4548bc918f8559a04d5f6e
Mike Chen(HuaXiao) <478222482@qq.com>
2023-06-24 15:44:08 +0900

    Time: 74 ms (6.95%), Space: 41.4 MB (96.09%) - LeetHub

--- 2637-promise-time-limit/2637-promise-time-limit.js
+++ 2637-promise-time-limit/2637-promise-time-limit.js
@@ -1,20 +1,9 @@
-/**
- * @param {Function} fn
- * @param {number} t
- * @return {Function}
- */
 function timeLimit(fn, t) {
   return async function (...args) {
-    return new Promise((resolve, reject) => {
-      const timerId = setTimeout(() => reject("Time Limit Exceeded"), t)
-      fn(...args)
-      .then(res => resolve(res))
-      .catch(reject)
-      .finally(() => clearTimeout(timerId))
+    const timeOutPromise = new Promise((_, reject) => {
+      setTimeout(() => reject("Time Limit Exceeded"), t)
     })
+    const returnedPromise = fn(...args) 
+    return Promise.race([timeOutPromise, returnedPromise])
   }
 }
-/**
- * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
- * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
- */
